6af2d84810c0a78a985533f5e07e7afcca82cd73
carlosalmonte04 <vacarlos_alm@hotmail.com>
2017-12-06 17:17:32 -0500

    --

--- controllers/sessions_controller.php
+++ controllers/sessions_controller.php
@@ -12,7 +12,7 @@
     public function store() {
       $conn = Db::getInstance();
       $content = $this->content_to_obj(file_get_contents('php://input'));
-      var_dump($content->Array[1]);
+      var_dump($content->Array[0]);
 
       $statement = $conn->prepare("SELECT username, password FROM users WHERE username=:username;");
       $statement->execute(array(':username' => $content->Array[1]));
