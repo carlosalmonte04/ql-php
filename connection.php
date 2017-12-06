b7c283ba76a5cfa4bc11f88cb26719e3ddf46e71
carlosalmonte04 <vacarlos_alm@hotmail.com>
2017-12-06 14:56:43 -0500

    attempt db connection 1

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        Db::$instance = new PDO('mysql:host=127.0.0.1;dbname=ql', 'root', '', $pdo_options);
+        Db::$instance = new PDO('mysql://becae04500022e:efb33258@us-cdbr-iron-east-05.cleardb.net/heroku_96c5d443b8d5d0f?reconnect=true', 'root', '', $pdo_options);
       }
       return self::$instance;
     }
